18f0e1c4ed1de92a03bde63cad89befe826b7f41
sgld365 <131637271+sgld365@users.noreply.github.com>
2026-05-29 15:46:06 +0800

    add: UniSDKInstance.java

---
+++ src/main/java/io/dcloud/feature/uniapp/UniSDKInstance.java
@@ -0,0 +1,11 @@
+package io.dcloud.feature.uniapp;
+
+import android.content.Context;
+
+public class UniSDKInstance {
+    private Context context;
+
+    public Context getContext() {
+        return context;
+    }
+}
