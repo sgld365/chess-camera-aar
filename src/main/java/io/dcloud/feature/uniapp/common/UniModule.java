c416a7701f69fa63d4052339c0611d684d048fbc
sgld365 <131637271+sgld365@users.noreply.github.com>
2026-05-29 15:32:32 +0800

    fix: add getContext() to UniModule stub

--- src/main/java/io/dcloud/feature/uniapp/common/UniModule.java
+++ src/main/java/io/dcloud/feature/uniapp/common/UniModule.java
@@ -5,6 +5,10 @@ import android.content.Context;
 public class UniModule {
     protected Context mUniSDKInstance;
 
+    public Context getContext() {
+        return mUniSDKInstance;
+    }
+
     public void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {}
     public void onActivityCreate() {}
     public void onActivityStart() {}
