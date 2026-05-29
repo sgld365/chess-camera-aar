8251d1af8ad20383aa56ff943bc475d8a9de8099
sgld365 <131637271+sgld365@users.noreply.github.com>
2026-05-30 00:51:28 +0800

    Restore DCloud stubs (excluded from AAR by build.gradle)

---
+++ src/main/java/io/dcloud/feature/uniapp/UniSDKInstance.java
@@ -0,0 +1,8 @@
+package io.dcloud.feature.uniapp;
+
+import android.content.Context;
+
+public class UniSDKInstance {
+    private Context context;
+    public Context getContext() { return context; }
+}
